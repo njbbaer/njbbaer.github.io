0c76b8908d3ee5c5f36fc8790e1e8569e69dfe04
Nate Baer <njbbaer@gmail.com>
2022-06-20 19:29:32 -0700

    Icon copies email to clipboard

--- index.js
+++ index.js
@@ -2,12 +2,38 @@ $(document).ready(function() {
   setCurrentJobDuration()
 });
 
-let setCurrentJobDuration = () => {
-  let startDate = new Date("2018-03-26");
-  let endDate = new Date();
+$("#copy-email").click(() => {
+  copyToClipboard("njbbaer@gmail.com");
+  showPopover($("#copy-email"), "Copied!");
+});
 
-  let years = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24 * 365.25));
-  let months = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24 * 30.44)) % 12;
-  $('#currentYears').text(years);
-  $('#currentMonths').text(months);
+const setCurrentJobDuration = () => {
+  const startDate = new Date("2018-03-26");
+  const endDate = new Date();
+
+  const years = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24 * 365.25));
+  const months = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24 * 30.44)) % 12;
+  $('#current-years').text(years);
+  $('#current-months').text(months);
+}
+
+const copyToClipboard = (content) => {
+  var copyTextarea = document.createElement("textarea");
+  copyTextarea.style.position = "fixed";
+  copyTextarea.style.opacity = "0";
+  copyTextarea.textContent = content;
+  document.body.appendChild(copyTextarea);
+  copyTextarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(copyTextarea);
+}
+
+const showPopover = (element, content) => {
+  const popover = new bootstrap.Popover(element, {
+    content: content
+  });
+  popover.show()
+  setTimeout(() => {
+    popover.dispose();
+  }, 1000);
 }
